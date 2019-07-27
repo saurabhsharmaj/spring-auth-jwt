c334b8be02c8c9dbe387084c063c4069d93ae263
saurabhsharmaj <saurabh.find@gmail.com>
2019-07-27 14:31:52 +0530

    test cases

---
+++ src/test/java/com/mir/lab/sec/oauth/jwt/config/security/GreetingControllerTest.java
@@ -0,0 +1,108 @@
+package com.mir.lab.sec.oauth.jwt.config.security;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.security.crypto.codec.Base64;
+import org.springframework.security.web.FilterChainProxy;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.mir.lab.sec.oauth.jwt.OAuth2ServerJwtApplication;
+import com.mir.lab.sec.oauth.jwt.web.UserController;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(classes = OAuth2ServerJwtApplication.class)
+public class GreetingControllerTest {
+
+	@Autowired
+	WebApplicationContext context;
+
+	@Autowired
+	private FilterChainProxy springSecurityFilterChain;
+
+	@InjectMocks
+	UserController controller;
+
+	private MockMvc mvc;
+
+	@Before
+	public void setUp() {
+		MockitoAnnotations.initMocks(this);
+		mvc = MockMvcBuilders.webAppContextSetup(context)
+				.addFilter(springSecurityFilterChain)
+				.build();
+	}
+
+	@Test
+	public void greetingUnauthorized() throws Exception {
+		// @formatter:off
+		mvc.perform(get("/users/me")
+				.accept(MediaType.APPLICATION_JSON))
+			.andExpect(status().isUnauthorized())
+			.andExpect(jsonPath("$.error", is("unauthorized")));
+		// @formatter:on
+	}
+
+	@Test
+	public void greetingAuthorized() throws Exception {
+		String authorization = "Basic "
+				+ new String(Base64.encode("clientId:secret".getBytes()));
+		String contentType = MediaType.APPLICATION_JSON + ";charset=UTF-8";
+
+		// @formatter:off
+		String content = mvc
+				.perform(
+						post("/oauth/token")
+								.header("Authorization", authorization)
+								.contentType(
+										MediaType.APPLICATION_FORM_URLENCODED)
+								.param("username", "mir")
+								.param("password", "pass")
+								.param("grant_type", "password")
+								.param("scope", "read write")
+								.param("client_id", "clientId")
+								.param("client_secret", "secret"))
+				.andExpect(status().isOk())
+				.andExpect(content().contentType(contentType))
+				.andExpect(jsonPath("$.access_token", is(notNullValue())))
+				.andExpect(jsonPath("$.token_type", is(equalTo("bearer"))))
+				.andExpect(jsonPath("$.refresh_token", is(notNullValue())))
+				.andExpect(jsonPath("$.expires_in", is(greaterThan(4000))))
+				.andExpect(jsonPath("$.scope", is(equalTo("read write"))))
+				.andReturn().getResponse().getContentAsString();
+	
+	    // @formatter:on
+
+		String accessToken = content.substring(17, 53);
+
+		// @formatter:off
+	    mvc.perform(get("/users/me")
+	            .header("Authorization", "Bearer " + accessToken))
+	            .andExpect(status().isOk())
+	            .andExpect(jsonPath("$.id", is(1)))
+	            .andExpect(jsonPath("$.content", is("Hello, mir!")));
+	    // @formatter:on
+	}
+
+}
