393392b46e5e336888c0aa71b3d8c9960c181cee
saurabhsharmaj <saurabh.find@gmail.com>
2019-07-27 13:50:39 +0530

    role permission

--- src/main/java/com/mir/lab/sec/oauth/jwt/config/security/WebSecurityConfiguration.java
+++ src/main/java/com/mir/lab/sec/oauth/jwt/config/security/WebSecurityConfiguration.java
@@ -1,19 +1,29 @@
 package com.mir.lab.sec.oauth.jwt.config.security;
 
+import javax.sql.DataSource;
+
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
-import javax.sql.DataSource;
+import org.springframework.security.web.session.HttpSessionEventPublisher;
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+	private static final String ROOT_PATTERN = "/**";
+	
     private final DataSource dataSource;
 
     private PasswordEncoder passwordEncoder;
@@ -51,6 +61,28 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
             ((JdbcDaoImpl) userDetailsService).setDataSource(dataSource);
         }
         return userDetailsService;
+    }
+    
+    @Bean
+    public SessionRegistry sessionRegistry() {
+        return new SessionRegistryImpl();
+    }
+
+    @Bean
+    public ServletListenerRegistrationBean<HttpSessionEventPublisher> httpSessionEventPublisher() {
+        return new ServletListenerRegistrationBean<HttpSessionEventPublisher>(new HttpSessionEventPublisher());
+    }
+    
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, ROOT_PATTERN).access("#oauth2.hasScope('read')")
+                .antMatchers(HttpMethod.POST, ROOT_PATTERN).access("#oauth2.hasScope('write')")
+                .antMatchers(HttpMethod.PATCH, ROOT_PATTERN).access("#oauth2.hasScope('write')")
+                .antMatchers(HttpMethod.PUT, ROOT_PATTERN).access("#oauth2.hasScope('write')")
+                .antMatchers(HttpMethod.DELETE, ROOT_PATTERN).access("#oauth2.hasScope('write')")
+                .and()
+                .sessionManagement().maximumSessions(1).sessionRegistry(sessionRegistry());;
     }
 
 }
--- src/main/java/com/mir/lab/sec/oauth/jwt/web/UserController.java
+++ src/main/java/com/mir/lab/sec/oauth/jwt/web/UserController.java
@@ -1,11 +1,12 @@
 package com.mir.lab.sec.oauth.jwt.web;
 
+import java.security.Principal;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.security.Principal;
 
 @RestController
 @RequestMapping("/users")
@@ -16,4 +17,9 @@ public class UserController {
         return ResponseEntity.ok(principal);
     }
 
+    @GetMapping("/all")
+    @PreAuthorize("hasRole('ROLE_USER')")
+    public ResponseEntity<String> get() {    	
+        return ResponseEntity.ok("TODO: Implement later to get userlist.");
+    }
 }
